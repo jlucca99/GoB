9d0ce43c2dc3bead5fdc6391d71a72da3b8b9492
Lucca Paletta <jlucca99@me.com>
2021-08-21 09:08:21 -0400

    Test commit

--- GoB/GameScene.swift
+++ GoB/GameScene.swift
@@ -89,21 +89,21 @@ class GameScene: SKScene {
     
     
     override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
-        
-        // Initialize _lastUpdateTime if it has not already been
-        if (self.lastUpdateTime == 0) {
-            self.lastUpdateTime = currentTime
-        }
-        
-        // Calculate time since last update
-        let dt = currentTime - self.lastUpdateTime
-        
-        // Update entities
-        for entity in self.entities {
-            entity.update(deltaTime: dt)
-        }
-        
-        self.lastUpdateTime = currentTime
+//        // Called before each frame is rendered
+//
+//        // Initialize _lastUpdateTime if it has not already been
+//        if (self.lastUpdateTime == 0) {
+//            self.lastUpdateTime = currentTime
+//        }
+//
+//        // Calculate time since last update
+//        let dt = currentTime - self.lastUpdateTime
+//
+//        // Update entities
+//        for entity in self.entities {
+//            entity.update(deltaTime: dt)
+//        }
+//
+//        self.lastUpdateTime = currentTime
     }
 }
